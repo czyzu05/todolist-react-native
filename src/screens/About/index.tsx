b6170ab304de87bfb4360a827d22682e149e7d68
MichalDziedzic <michaldziedzic8@gmail.com>
2020-05-27 10:03:34 +0200

    fix home & about

--- src/screens/About/index.tsx
+++ src/screens/About/index.tsx
@@ -1,27 +1,75 @@
-import React, { FC } from 'react';
-import { Button, View } from 'react-native';
-import styled from 'styled-components/native';
+import React, { FC } from "react";
+import { Button, View, Image, Text } from "react-native";
+import styled from "styled-components/native";
 
-import Colors from '../../constans/Colors';
+import Colors from "../../constans/Colors";
 
 const WelcomeText = styled.Text`
-    margin: 120px 20px;
-    font-size: 16px;
-    color: ${Colors.black};
+  margin: 120px 20px;
+  font-size: 16px;
+  color: ${Colors.black};
 `;
-
+const CustomImage = styled.Image`
+  width: 40%;
+  height: 70%;
+  margin-left: 10px;
+  margin-right: 10px;
+`;
+const WelcomeTextHeader = styled.Text`
+  font-size: 16px;
+  color: ${Colors.black};
+  padding: 30px 10px;
+  text-align: center;
+`;
+const WelcomeTextAbout = styled.Text`
+  font-size: 16px;
+  color: ${Colors.black};
+  padding: 5px 10px;
+`;
+const Container = styled.View`
+  flex: 1;
+  background-color: papayawhip;
+`;
+const Titlerr = styled.Text`
+  color: pink;
+`;
+const HeaderAbout = styled.View`
+  flex: 2;
+  padding-top: 20px;
+  align-items: flex-start;
+  justify-content: center;
+`;
+const SectionDescribe = styled.View`
+  flex: 3;
+`;
+const FooterAbout = styled.View`
+  flex: 2;
+  align-items: flex-end;
+  justify-content: center;
+`;
 interface IAboutProps {}
 
-const About = ({navigation}) => {
-    return (
-        <View>
-            <WelcomeText>To jest TodoList</WelcomeText>
-            <Button title="Home screen" onPress={() => {
-                navigation.navigate('Home');
-            }}
-            />
-        </View>
-    );
+const About = ({ navigation }) => {
+  return (
+    <Container>
+      <HeaderAbout>
+        {<CustomImage source={require("../../assets/logo.png")} />}
+      </HeaderAbout>
+      <SectionDescribe>
+        <WelcomeTextHeader>About</WelcomeTextHeader>
+        <WelcomeTextAbout>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+          elementum urna diam, in dictum libero volutpat et. Fusce a maximus
+          leo, a ultricies nisi. Integer non mollis velit. Donec vitae augue
+          vitae ante porta sollicitudin in id leo. Orci varius natoque penatibus
+          et magnis dis parturient montes, nascetur ridiculus mus.
+        </WelcomeTextAbout>
+      </SectionDescribe>
+      <FooterAbout>
+        {<CustomImage source={require("../../assets/logo.png")} />}
+      </FooterAbout>
+    </Container>
+  );
 };
 
 export default About;
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,39 +1,84 @@
-import React, { FC } from 'react';
-import { Button, View, Image, Text } from 'react-native';
-import styled from 'styled-components/native';
-import { useNavigation } from '@react-navigation/native';
+import React, { FC } from "react";
+import { Button, View, Image, Text } from "react-native";
+import styled from "styled-components/native";
+import { useNavigation } from "@react-navigation/native";
 
-import Colors from '../../constans/Colors';
+import Colors from "../../constans/Colors";
 
 const WelcomeText = styled.Text`
-    margin: 120px 20px;
-    font-size: 16px;
-    color: ${Colors.black};
+  margin: 120px 20px;
+  font-size: 16px;
+  color: ${Colors.black};
 `;
 
 const CustomImage = styled.Image`
-    width: 50px;
-    height: 50px;
-    margin: 50px;
+  width: 100%;
+  height: 80%;
+`;
+const WelcomeTextHeader = styled.Text`
+  font-size: 16px;
+  color: ${Colors.black};
+  padding: 30px 10px;
+  text-align: center;
+`;
+const WelcomeTextHome = styled.Text`
+  font-size: 16px;
+  color: ${Colors.black};
+  padding: 10px 10px;
+`;
+const WrapperHome = styled.View`
+  flex: 1;
+  background-color: papayawhip;
 `;
 
-interface IHomeProps { }
+const HeaderHome = styled.View`
+  flex: 1;
+  flex-direction: row;
+`;
+const ImgWrapper = styled.View`
+  flex: 1;
+
+  width: 50%;
+  justify-content: center;
+`;
+const SectionHome = styled.View`
+  flex: 1;
+  background-color: papayawhip;
+  align-items: center;
+`;
+const TitleHeaderHome = styled.View`
+  flex: 1;
+  width: 50%;
+  align-items: center;
+`;
+
+interface IHomeProps {}
 
 const Home: FC<IHomeProps> = (props) => {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    return (
-        <View>
-            <WelcomeText>To jest home screen 2 xDsss</WelcomeText>
-            <CustomImage
-                source={require('../../assets/logo.png')}
-            />
-            <Button title="TodoList screen" onPress={() => {
-                navigation.navigate('TodoList');
-            }}
-            />
-        </View>
-    );
+  return (
+    <WrapperHome>
+      <HeaderHome>
+        <ImgWrapper>
+          {<CustomImage source={require("../../assets/logo.png")} />}
+        </ImgWrapper>
+        <TitleHeaderHome>
+          <WelcomeTextHeader>WELCOM IN MY TODO APP</WelcomeTextHeader>
+        </TitleHeaderHome>
+      </HeaderHome>
+      <SectionHome>
+        <WelcomeTextHeader>EXAMPLE HEADER </WelcomeTextHeader>
+        <WelcomeTextHome>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+          elementum urna diam, in dictum libero volutpat et. Fusce a maximus
+          leo, a ultricies nisi. Integer non mollis velit. Donec vitae augue
+          vitae ante porta sollicitudin in id leo. Orci varius natoque penatibus
+          et magnis dis parturient montes, nascetur ridiculus mus.
+        </WelcomeTextHome>
+      </SectionHome>
+    </WrapperHome>
+  );
 };
 
 export default Home;
